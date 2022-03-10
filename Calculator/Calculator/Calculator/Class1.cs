9e41d03f7371af7c43869357e0942dec5aa735f5
rafalgorniak <234753@edu.p.lodz.pl>
2022-03-10 13:15:52 +0100

    calculator

---
+++ Calculator/Calculator/Calculator/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalculatorNS
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return x / y;
+        }
+    }
+}
--- README.md
+++
@@ -1,8 +0,0 @@
-# Programming Technology Lab
-
-## Team
-
-| Name Surname (initials) | GUID                                     |
-| ----------------------- | ---------------------------------------- |
-| Kacper Królewiak        | `{ca62c321-091e-41a6-957e-48eeb94fd647}` |
-| Rafał Górniak           | `{9899f295-8ca9-4497-be0a-f83d12e8799e}` |
